58e7a9efbde411384e738e059c3d0c2eb3d88217
Pete Seeling <peteseeling@gmail.com>
2022-01-07 13:40:30 -0600

    finished final

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ const signUpPassword = document.getElementById('sign-up-password');
 // if user currently logged in, redirect
 redirectIfLoggedIn();
 
-
-
 signUpForm.addEventListener('submit', async(event)=>{
     event.preventDefault();
     const user = await signupUser(signUpEmail.value, signUpPassword.value);
