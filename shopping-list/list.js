5249e6b78ef688d34d09cded9d281b0cf5b4d1b7
Pete Seeling <peteseeling@gmail.com>
2022-01-06 18:59:21 -0600

    Dom Elements grabbed

--- shopping-list/list.js
+++ shopping-list/list.js
@@ -1,9 +1,16 @@
 import { checkAuth, logout } from '../fetch-utils.js';
 
-
+const itemForm = document.querySelector('item-form');
+const addButton = document.querySelector('#add-button');
+const deleteItemButton = document.querySelector('#delete-item');
+const deleteListButton = document.querySelector('#delete-list');
+const itemListEl = document.querySelector('.list-items');
+const logoutButton = document.getElementById('logout');
 checkAuth();
 
-const logoutButton = document.getElementById('logout');
+
+
+
 
 logoutButton.addEventListener('click', () => {
     logout();
